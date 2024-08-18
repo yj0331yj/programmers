3350d839892814785a87000757cfbcfe00cc04d2
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-18 22:57:24 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,18 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+
+    def dfs(cur_v):
+        visited[cur_v] = True
+        for next_v in range(n):
+            if computers[cur_v][next_v] == 1:
+                if not visited[next_v]:
+                    dfs(next_v)
+                    
+    # 모든 노드에 대해서 dfs() 실행 시도 -> 실행되면 answer ++
+    for cur_v in range(n):
+        if not visited[cur_v]:
+            dfs(cur_v)
+            answer += 1
+
+    return answer
