68e357a9eafc32bfd1c563f7c0fb8e61f6d6119c
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-19 09:20:23 +0900

    Create 네트워크_bfs.py
    bfs 방식으로 풀이해보기

---
+++ 프로그래머스/3/43162. 네트워크/네트워크_bfs.py
@@ -0,0 +1,27 @@
+# bfs 방식으로 풀이
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    def bfs(start_v):
+        q = deque()
+        q.append(start_v)
+        visited[start_v] = True
+
+        while q:
+            cur_v = q.popleft()
+            for next_v in range(n):
+                if computers[cur_v][next_v] == 1:
+                    if not visited[next_v]:
+                        q.append(next_v)
+                        visited[next_v] = True
+                        bfs(next_v)
+                    
+    for cur_v in range(n):
+        if not visited[cur_v]:
+            bfs(cur_v)
+            answer += 1
+            
+    return answer
