00918320e048c6a0630d238cb0c737a3359d0f66
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-20 14:22:36 +0900

    [level 2] Title: 타겟 넘버, Time: 1010.76 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,22 @@
 from collections import deque
+
 def solution(numbers, target):
     answer = 0
     n = len(numbers)
     q = deque()
-    q.append([-numbers[0], 0])
-    q.append([+numbers[0], 0])
+    q.append((0, 0))  # 초기 설정: (인덱스, 현재까지의 합)
 
     while q:
-        num, i = q.popleft()
-        if i + 1 == n:
-            if num == target:
-                answer += 1
+        i, value = q.popleft()
+        # 모든 숫자를 사용한 경우 현재까지의 합이 목표값과 같다면 답을 1 증가
+        if i == n and value == target:
+            answer += 1
+        # 모든 숫자를 사용했지만 목표값과 다를 경우 pass
+        elif i == n:
+            pass
+        # 아직 모든 숫자를 사용하지 않은 경우
         else:
-            q.append([num - numbers[i+1], i+1])
-            q.append([num + numbers[i+1], i+1])
-        
+            q.append((i+1, value + numbers[i])) # 현재 숫자를 더한 경우를 큐에 추가
+            q.append((i+1, value - numbers[i])) # 현재 숫자를 뺀 경우를 큐에 추가
+    
     return answer
