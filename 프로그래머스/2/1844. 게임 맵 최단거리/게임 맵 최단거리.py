9e224a9743bc06f1795dab39bb3beb401540d407
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-21 14:48:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.20 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,27 +1,30 @@
 from collections import deque
 
 def solution(maps):
-    dr = [1, 0, -1, 0]
-    dc = [0, 1, 0, -1]
+    answer = -1
     n = len(maps)
     m = len(maps[0])
-    queue = deque([(0, 0, 1)])
-
-    while queue:
-        x, y, d = queue.popleft()
-        if x == n-1 and y == m-1:
-            return maps[x][y]
-
-        for i in range(4):
-            nx = x + dr[i]
-            ny = y + dc[i]
-
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] == 1 or maps[nx][ny] > d + 1:
-                    maps[nx][ny] = d + 1
-                    if nx == m - 1 and ny == n - 1:
-                        return d + 1
-
-                    queue.append((nx, ny, d + 1))
-
-    return -1
+    dr = [1, 0, -1, 0]
+    dc = [0, 1, 0, -1]
+    visited = [[False]*m for _ in range(n)]
+    q = deque()
+    
+    if maps[0][0] == 1:
+        q.append((0,0))
+        visited[0][0] = True
+        
+        while q:
+            cur_r, cur_c = q.popleft()
+            if cur_r == n-1 and cur_c == m-1:
+                return maps[cur_r][cur_c]
+            
+            for i in range(4):
+                next_r = cur_r + dr[i]
+                next_c = cur_c + dc[i]
+                if 0 <= next_r < n and 0 <= next_c < m and maps[next_r][next_c] == 1:
+                    if not visited[next_r][next_c]:
+                        q.append((next_r, next_c))
+                        visited[next_r][next_c] = True
+                        maps[next_r][next_c] = maps[cur_r][cur_c] + 1
+    
+    return answer
