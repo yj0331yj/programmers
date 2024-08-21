136c0e54dc15d1ac23be320b48ecbc58aa7878a4
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-21 13:12:40 +0900

    [level 3] Title: 네트워크, Time: 1.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,25 @@
+from collections import deque
+
 def solution(n, computers):
     answer = 0
     visited = [False] * n
     
-    def dfs(cur_v):
-        visited[cur_v] = True
-        for next_v in range(n):
-            if computers[cur_v][next_v] == 1: # 연결됐을 경우(1인 경우)만 실행
-                if not visited[next_v]:
-                    dfs(next_v)
-                    
-    # 모든 노드에 대해서 dfs() 실행 시도 -> 실행되면 answer ++
+    def bfs(start_v):
+        q = deque()
+        q.append(start_v)
+        visited[start_v] = True
+        
+        while q:
+            cur_v = q.popleft()
+            for next_v in range(n):
+                if computers[cur_v][next_v] == 1:
+                    if not visited[next_v]:
+                        q.append(next_v)
+                        visited[next_v] = True
+                        bfs(next_v)
+                        
     for cur_v in range(n):
         if not visited[cur_v]:
-            dfs(cur_v)
+            bfs(cur_v)
             answer += 1
-            
     return answer
